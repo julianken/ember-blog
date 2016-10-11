9e50361473709fb2c56af90021ddabfc8184364e
julianken <julian.kennon.d@gmail.com>
2016-10-11 01:32:40 -0700

    mobile menu functionality factory

--- app/components/header-bar.js
+++ app/components/header-bar.js
@@ -4,17 +4,32 @@ import $ from 'jquery';
 export default Ember.Component.extend({
   activeClass: '',
   isActive: false,
+  init: function() {
+    this._super();
+    Ember.$(window).resize(function(){
+      var width = $(window).width();
+      if (width > 768 && this.get('isActive') == true) {
+        this.set('activeClass', '');
+        Ember.$('.app-content').removeClass('active-mobile-body');
+        Ember.$('.header').removeClass('active-mobile-header');
+        this.set('isActive', false);
+      }
+    }.bind(this));
+  },
   actions: {
     toggleMobileMenu() {
       if (this.isActive === false) {
         this.set('activeClass', 'active');
+        Ember.$('.app-content').addClass('active-mobile-body');
+        Ember.$('.header').addClass('active-mobile-header');
         this.set('isActive', true);
+        this.get('')
       } else {
         this.set('activeClass', '');
+        Ember.$('.app-content').removeClass('active-mobile-body');
+        Ember.$('.header').removeClass('active-mobile-header');
         this.set('isActive', false);
       }
-      Ember.$('.app-content').toggleClass('active-mobile-body');
-      Ember.$('.header').toggleClass('active-mobile-header');
     }
   }
 });
