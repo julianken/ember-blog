fb06a74b988db0dde52266a371859971240bce23
julianken <julian.kennon.d@gmail.com>
2016-10-11 15:48:12 -0700

    computed title and observer for mobile

--- app/components/header-bar.js
+++ app/components/header-bar.js
@@ -4,17 +4,28 @@ import $ from 'jquery';
 export default Ember.Component.extend({
   activeClass: '',
   isActive: false,
+  uncomputedNavTitle: '',
+  mobileNavTitle: Ember.computed('uncomputedNavTitle', function(){
+    var title = this.get('uncomputedNavTitle');
+    title = title.split('.')[0];
+    title = title.charAt(0).toUpperCase() + title.slice(1);
+    return title;
+  }),
+  mobileNavTitleChanged: Ember.observer('router.currentRouteName', function(){
+    this.set('uncomputedNavTitle', this.get('router.currentRouteName'));
+  }),
   init: function() {
     this._super();
     Ember.$(window).resize(function(){
       var width = $(window).width();
-      if (width > 768 && this.get('isActive') == true) {
+      if (width > 768 && this.get('isActive') === true) {
         this.set('activeClass', '');
         Ember.$('.app-content').removeClass('active-mobile-body');
         Ember.$('.header').removeClass('active-mobile-header');
         this.set('isActive', false);
       }
     }.bind(this));
+    this.set('uncomputedNavTitle', this.get('router.currentRouteName'));
   },
   actions: {
     toggleMobileMenu() {
@@ -23,7 +34,6 @@ export default Ember.Component.extend({
         Ember.$('.app-content').addClass('active-mobile-body');
         Ember.$('.header').addClass('active-mobile-header');
         this.set('isActive', true);
-        this.get('')
       } else {
         this.set('activeClass', '');
         Ember.$('.app-content').removeClass('active-mobile-body');
@@ -37,6 +47,6 @@ export default Ember.Component.extend({
       Ember.$('.header').removeClass('active-mobile-header');
       Ember.$(window).scrollTop(0);
       this.set('isActive', false);
-    }
+    },
   }
 });
