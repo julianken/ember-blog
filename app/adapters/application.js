f7ed617696beac2c07c4eaef8814e7a2c1573770
julianken <julian.kennon.d@gmail.com>
2017-01-21 14:23:30 -0700

    style and resume updates

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-  host: 'https://poligap-api.herokuapp.com'
+	host: 'http://138.197.204.86:3000'
 });
--- app/routes/resume.js
+++ app/routes/resume.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function(){
-    return 'I come from a diverse background and have a particular set of skills <img class="neesons" src="img/neesons.png" alt="Liam Neeson Emoji"> that I thin would be beneficial etc etc.';
+  model: () => {
+    return 'I come from a diverse background and have a particular set of skills <img class="neesons" src="img/neesons.png" alt="Liam Neeson Emoji"> that I think would be beneficial etc etc.';
   }
 });
