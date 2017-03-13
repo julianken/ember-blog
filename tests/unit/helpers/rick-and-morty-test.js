3477154bb69ee9072fd1e5579dc5a325a37be8a9
julianken <julian.kennon.d@gmail.com>
2017-03-13 14:33:10 -0700

    add rick and morty svg helper, and animations for them

---
+++ tests/unit/helpers/rick-and-morty-test.js
@@ -0,0 +1,10 @@
+import { rickAndMorty } from 'ember-blog/helpers/rick-and-morty';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | rick and morty');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = rickAndMorty([42]);
+  assert.ok(result);
+});
